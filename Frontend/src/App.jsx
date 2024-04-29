93f761e7a6d716877373138812dea8eed0d0ae62
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-04-29 19:37:27 +0200

    implemented the ThemeProvider (mui)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import {Hero} from './components/landingPage/Hero';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ffffff', 
+      contrastText: '#000', 
+    },
+    secondary: {
+      main: '#f50057', 
+    },
+    background: {
+      default: '#121212', 
+      paper: '#1e1e1e',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: 'rgba(255, 255, 255, 0.7)',
+    },
+  },
+  typography: {
+    fontFamily: '"Helvetica Neue", Arial, sans-serif', 
+    h3: {
+      fontWeight: 700, 
+    },
+    h5: {
+      fontWeight: 500, 
+    },
+    body1: {
+      fontWeight: 400, 
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: 700, 
+        },
+      },
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Hero />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
