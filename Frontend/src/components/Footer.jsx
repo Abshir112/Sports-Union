04aa074fb29ffe2155d1440d344dd5fa98c77f80
JwanMardini <jwan.99.mardini@gmail.com>
2024-04-30 18:35:18 +0200

    footer is done

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,28 +1,49 @@
 import React from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Link } from "@mui/material";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedinIcon from '@mui/icons-material/LinkedIn';
-import Divider from '@mui/material/Divider';
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+
+
 
 const Footer = () => {
-    return (
-        <Container component="footer" style={{display: "flex", justifyContent: "space-between"}}>
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+    return (<>
+        <hr />
+        <Container maxWidth="lg" component="footer" style={{display: "flex", justifyContent: "space-between"}}>
             <Box>
-                <Typography variant="h6" > Contact Us </Typography>
-                <InstagramIcon></InstagramIcon>
-                <FacebookIcon></FacebookIcon>
-                <LinkedinIcon></LinkedinIcon>
-            </Box>
-            <Box>
-                <Typography variant="h6">Contact Information</Typography>
+                <Typography gutterBottom variant="h6" fontSize={isSmallScreen ? "medium" : "large"} align="center" > Contact Us </Typography>
+                <Box>
+                    <Link href="#">
+                        <InstagramIcon fontSize={isSmallScreen ? "small" : "large"} sx={{ mr: isSmallScreen ? 3 : 5 }} />
+                    </Link>
+
+                    <Link href="#">
+                        <FacebookIcon fontSize={isSmallScreen ? "small" : "large"} sx={{ mr: isSmallScreen ? 3 : 5 }}/>
+                    </Link>
+                       
+                    <Link href="#">
+                        <LinkedinIcon fontSize={isSmallScreen ? "small" : "large"} ></LinkedinIcon>
+                    </Link>
+                </Box>
 
             </Box>
             <Box>
-                <Typography variant="h6">Office Hours</Typography>
+                <Typography gutterBottom variant="h6" fontSize={isSmallScreen ? "medium" : "large"} align="center">Contact Information</Typography>
+                <Typography variant="body1" fontSize="small" align="center" >Place: Ground Floor In <br /> Building 15 At HÖGSKOLAN <br /> KRISTIANSTAD </Typography>
+
+            </Box>
+            <Box>
+                <Typography gutterBottom variant="h6" fontSize={isSmallScreen ? "medium" : "large"} align="center">Office Hours</Typography>
+                <Typography variant="body1" fontSize="small"  align="center" > Mon to Fri: 09:00 - 03:00 <br /> Sat and Sun: Holiday </Typography>
             </Box>
         </Container>
+        </>
     );
+    
 }
 
 export default Footer;
