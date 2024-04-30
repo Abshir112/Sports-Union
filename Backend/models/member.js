8ea88f758295d5347d50e34bc4e82e1a52ba8ca6
Alloush95 <mohamad.alloush0090@stud.hkr.se>
2024-04-30 09:46:47 +0200

    Updated the member schema

--- Backend/models/member.js
+++ Backend/models/member.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const memberSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    personalNumber: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
+    phone: { type: String, required: true, unique: true},
+    personalNumber: { type: String, required: true, unique: true},
+    password: { type: String, required: true },
 }, { collection: 'Member' });
 
 const Member = mongoose.model('Member', memberSchema);
