df4fcd3439f0cba16c6addcd753e3a3a0f7a3ee8
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-01 11:59:49 +0200

    naming updates to match the structure

---
+++ Backend/routes/member.routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { signUp, signIn } from '../controllers/auth.user.js';
+import { connectDB } from '../controllers/db.connection.js';
+import { getAllMembers } from '../controllers/user.controller.js';
+
+
+const memberRouter = express.Router();
+
+
+// Route handler to get all members
+memberRouter.get('/', getAllMembers);
+
+// Route handler to create a new member
+memberRouter.post('/sign-up',signUp);
+
+//  Route handler to login a member
+memberRouter.post('/sign-in', signIn);
+
+export default memberRouter;
