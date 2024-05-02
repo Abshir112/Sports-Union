a6df1555d9bf1b02b99a2458b6674ea7d1532d8c
JwanMardini <jwan.99.mardini@gmail.com>
2024-05-02 12:36:22 +0200

    added form validation

--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { Container, TextField, Button, Typography, Link } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
+import React, { useState } from 'react';
+import { Container, TextField, Button, Typography, Grid} from '@mui/material';
 
 function SignIn() {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  });
+  
+  const [errors, setErrors] = useState({});
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+  
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.email = values.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? "" : "Email is not valid";
+    tempErrors.password = values.password.length >= 6 ? "" : "Password must be at least 6 characters long";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === "");
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      console.log('Form is valid');
+
+    } else {
+      console.log('Form is invalid');
+    }
+  };
   return (
-    <Container component="main" maxWidth="md" style={{marginTop: '20px', marginBottom:'40px'}}>
+    <Container component="main" maxWidth="md" style={{ marginTop: '20px', marginBottom: '40px' }}>
       <Grid container spacing={2}>
         {/* Form Section */}
-        <Grid item xs={12} md={6} style={{marginTop: '100px'}}>
-          <Typography component="h1" variant="h5" align='center'>
+        <Grid item xs={12} md={6} style={{ marginTop: '100px' }}>
+          <Typography component="h1" variant="h5" align="center">
             Log In
           </Typography>
-          <form style={{ marginTop: '10px' }} method='POST'>
+          <form style={{ marginTop: '10px' }} method='POST' onSubmit={handleSubmit}>
             <TextField
               variant="standard"
               margin="normal"
@@ -20,8 +50,12 @@ function SignIn() {
               fullWidth
               id="email"
               label="Email"
-              name="Email"
-              autoComplete="ssn"
+              name="email"
+              autoComplete="email"
+              value={values.email}
+              onChange={handleChange}
+              error={!!errors.email}
+              helperText={errors.email}
             />
             <TextField
               variant="standard"
@@ -33,6 +67,10 @@ function SignIn() {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={values.password}
+              onChange={handleChange}
+              error={!!errors.password}
+              helperText={errors.password}
             />
             <Button
               type="submit"
