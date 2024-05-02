7681bceba9757439995bc1fce07fe7c9f4ca2159
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-02 10:10:22 +0200

    added the controllers to update and delete activities

--- Backend/controllers/activity.controller.js
+++ Backend/controllers/activity.controller.js
@@ -1,21 +1,61 @@
 import Activity from "../models/activity.model.js";
 
+
 // Function to retrieve all activities
-export const getAllActivities = async () => {
+export const getAllActivities = async (req, res) => {
     try {
         const activities = await Activity.find();
-        return activities;
+        res.json(activities);
     } catch (error) {
-        console.error("Error fetching activities:", error);
+        console.error('Error fetching activities:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
 
-// Function to create a new activity with validation
-export const createActivity = async (activityData) => {
+// Function to create a new activity
+export const createActivity = async (req, res) => {
     try {
-        const newActivity = await Activity.create(activityData);
-        return newActivity;
+        const newActivity = await Activity.create(req.body);
+        res.status(201).json(newActivity);
     } catch (error) {
-        throw new Error('Failed to add the new activity', error); // Rethrow the error for handling in the caller
+        console.error('Failed to create activity:', error);
+        res.status(500).json({ error: 'Failed to create activity' });
     }
 };
+
+// Function to update an activity
+export const updateActivity = async (req, res) => {
+    const { id } = req.params;
+    const { activityName, scheduledTime, maxParticipants } = req.body;
+    try {
+        const updatedActivity = await Activity.findByIdAndUpdate(
+            id,
+            { activityName, scheduledTime, maxParticipants },
+            { new: true }
+        );
+        if (!updatedActivity) {
+            return res.status(404).json({ error: 'Activity not found' });
+        }
+        res.json(updatedActivity);
+    } catch (error) {
+        console.error('Failed to update activity:', error);
+        res.status(500).json({ error: 'Failed to update activity' });
+    }
+};
+
+
+// Function to delete an activity
+export const deleteActivity = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedActivity = await Activity.findByIdAndDelete(id);
+        if (!deletedActivity) {
+            return res.status(404).json({ error: 'Activity not found' });
+        }
+        res.json({ message: 'Activity deleted successfully' });
+    } catch (error) {
+        console.error('Failed to delete activity:', error);
+        res.status(500).json({ error: 'Failed to delete activity' });
+    }
+};
+
