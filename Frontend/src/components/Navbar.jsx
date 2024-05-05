b883ee01f92e782912ab71bff6e9b75ab02f44c7
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2024-05-05 04:23:42 +0200

    added log out button when user log in

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link as RouterLink } from 'react-router-dom';
 import {
   AppBar,
@@ -11,11 +11,19 @@ import Link from "@mui/material/Link";
 import DrawerComp from "./Drawer";
 import Logo from "./Logo";
 import { ButtonHandler } from "./Button";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
   
 function Navbar() {
     const theme = useTheme();
     const isMatch = useMediaQuery(theme.breakpoints.down("md"));
-  
+    const { logout } = useLogout();
+    const {user} = useAuthContext();
+
+    const handleLogout = () => {
+        logout();
+    };
+
     return (
       <React.Fragment>
 
@@ -42,13 +50,13 @@ function Navbar() {
                       
                     }}
                 >
-                        <Link component={RouterLink} to="/" underline="hover" mr={1}>
+                        <Link component={RouterLink} to={user ? "/events" : "/"} underline="hover" mr={1}>
                             Home
                         </Link>
                         <Link component={RouterLink} to="/about" underline="hover" mr={1}>
                             About
                         </Link>
-                        <Link component={RouterLink} to="/contact" underline="hover" mr={1}>
+                        <Link component={RouterLink} to="/events" underline="hover" mr={1}>
                             Events
                         </Link>
                         <Link component={RouterLink} to="/Members" underline="hover" mr={1}>
@@ -57,6 +65,16 @@ function Navbar() {
                         <Link component={RouterLink} to="/Stories" underline="hover">
                             Stories
                         </Link>
+
+                        {
+                            user ? (
+                                <>
+                                    <ButtonHandler title="Logout" link="/signIn" onClick={handleLogout} />
+                                </>
+                            ) : (
+                              null
+                            )
+                        }
                 </Box>
               </>
             )}
@@ -64,5 +82,5 @@ function Navbar() {
         </AppBar>
       </React.Fragment>
     );
-  };
+  }
 export default Navbar;
