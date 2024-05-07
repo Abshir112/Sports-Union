475422e48211ac7a53d9adb27f2ccfde78191736
Alloush95 <mohamad.alloush0090@stud.hkr.se>
2024-05-07 19:03:38 +0200

    added event route

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './routes/user.routes.js';
 import activityRouter from './routes/activity.routes.js';
 import {connectDB} from './controllers/db.connection.js';
+import eventrRouter from './routes/event.routes.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
@@ -23,6 +24,8 @@ app.use(express.json());
 app.use('/users', userRouter);
 
 app.use('/activity', activityRouter);
+
+app.use('/event', eventrRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
