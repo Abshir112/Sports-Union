3b8a5d1e198fe60bd35e3ca1c857f006a7367eaf
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-07 19:17:50 +0200

    fixed the naming of the events route

--- Backend/routes/event.routes.js
+++ Backend/routes/event.routes.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import {createEvent, getAllEvents, getEventById, updateEvent, deleteEvent } from '../controllers/event.controller.js';
 
-const eventrRouter = express.Router();
+const eventRouter = express.Router();
 // Create route for the Event
-eventrRouter.post('/create-event', createEvent);
+eventRouter.post('/create-event', createEvent);
 
 // get all events from the database
-eventrRouter.get('/get-events', getAllEvents);
+eventRouter.get('/get-events', getAllEvents);
 
 // get a single event by id
-eventrRouter.get('/get-event/:id', getEventById);
+eventRouter.get('/get-event/:id', getEventById);
 
 // update a event by id
-eventrRouter.put('/update-event/:id', updateEvent);
+eventRouter.put('/update-event/:id', updateEvent);
 
 // delete a event by id
-eventrRouter.delete('/delete-event/:id', deleteEvent);
+eventRouter.delete('/delete-event/:id', deleteEvent);
 
 
-export default eventrRouter;
+export default eventRouter;
