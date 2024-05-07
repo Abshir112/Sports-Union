90ab58323b29d41ba21c5ae23f190b81c8ef66e9
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-05-07 17:23:55 +0200

    added time and date separately

--- Frontend/src/components/EventCard.jsx
+++ Frontend/src/components/EventCard.jsx
@@ -2,32 +2,37 @@ import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box, Grid } from '@mui/material';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
 
 const EventCard = (props) => {
   return (
     <Card variant="outlined" sx={{  m: 2, boxShadow: 3, p: 2 }}>
-      <Grid container >
-        <Grid item xs={12} sm={6} md={6} lg={4}>
+      <Grid container spacing={12} >
+        <Grid item xs={12} sm={6} md={6} lg={4} >
           <CardMedia
             component="img"
             image={props.image || "https://via.placeholder.com/150"}
-            alt="Event image"
-            sx={{ width: '100%', height: 'auto', maxWidth: 300, objectFit: 'cover', ml: { xs: 0, sm: 2 }, borderRadius: 2, ml: 10}}  
+            alt={props.title + "image"}
+            sx={{ width: '100%', height: 'auto', maxWidth: 300, objectFit: 'cover', borderRadius: 5}}  
           />
         </Grid>
         <Grid item xs={12} sm={6} md={6} lg={8}>
-          <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', marginLeft: '0'}}>
             <Box sx={{ color: 'orange', fontWeight: '900' }}>
-              <Typography gutterBottom variant="h5" component="div">
+              <Typography gutterBottom variant="h4" component="div">
                 {props.title}
               </Typography>
-              <Box display="flex" alignItems="center" sx={{ color: 'white', p: 1 }}>
-                <AccessTimeIcon fontSize="small" sx={{ mr: 1 }} /> 
-                <Typography variant="body2" component="span">
-                  {props.date} /
+              <Box display="flex" alignItems="center" sx={{ color: 'white', paddingBottom: '10px'}}>
+                <DateRangeOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+                <Typography variant="body1" component="span" letterSpacing={1} marginRight={2}>
+                  {props.date}
+                </Typography>
+                <AccessTimeIcon fontSize="small" sx={{ ml: 1}} /> 
+                <Typography variant="body1" component="span" marginRight={2} sx={{ mx: 1 }}>
+                {props.time}
                 </Typography>
                 <PlaceOutlinedIcon fontSize="small" sx={{ mx: 1 }} /> 
-                <Typography variant="body2" component="span">
+                <Typography variant="body1" component="span">
                   {props.location}
                 </Typography>
               </Box>
