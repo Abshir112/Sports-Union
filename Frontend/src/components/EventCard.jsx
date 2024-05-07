78e0dc699d7d4875fb7b2748d392255947c7cb4f
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-05-07 12:17:14 +0200

    eventCard component

---
+++ Frontend/src/components/EventCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box, Grid } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+
+const EventCard = (props) => {
+  return (
+    <Card variant="outlined" sx={{  m: 2, boxShadow: 3, p: 2 }}>
+      <Grid container >
+        <Grid item xs={12} sm={6} md={6} lg={4}>
+          <CardMedia
+            component="img"
+            image={props.image || "https://via.placeholder.com/150"}
+            alt="Event image"
+            sx={{ width: '100%', height: 'auto', maxWidth: 300, objectFit: 'cover', ml: { xs: 0, sm: 2 }, borderRadius: 2, ml: 10}}  
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={8}>
+          <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <Box sx={{ color: 'orange', fontWeight: '900' }}>
+              <Typography gutterBottom variant="h5" component="div">
+                {props.title}
+              </Typography>
+              <Box display="flex" alignItems="center" sx={{ color: 'white', p: 1 }}>
+                <AccessTimeIcon fontSize="small" sx={{ mr: 1 }} /> 
+                <Typography variant="body2" component="span">
+                  {props.date} /
+                </Typography>
+                <PlaceOutlinedIcon fontSize="small" sx={{ mx: 1 }} /> 
+                <Typography variant="body2" component="span">
+                  {props.location}
+                </Typography>
+              </Box>
+              <Typography variant="body2">
+                {props.description}
+              </Typography>
+            </Box>
+            <CardActions sx={{ justifyContent: 'start' }}>
+              <Button size="small" variant="contained" sx={{ backgroundColor: 'black', color: 'white' }}>
+                Reserve
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
+
+export default EventCard;
