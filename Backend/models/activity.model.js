f89d96e432fc5c76914808f5bca4c53222f22cfa
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-07 18:50:28 +0200

    Added the location and description fields tot he activitiy

--- Backend/models/activity.model.js
+++ Backend/models/activity.model.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const activitySchema = new mongoose.Schema({
     activityName: { type: String, required: true },
-    scheduledTime: { type: Date, required: true },
-    maxParticipants: { type: Number, required: true }
+    date: { type: Date, required: true },
+    maxParticipants: { type: Number, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    time: { type: String, required: true }
+
 }, { collection: 'Activity' });
 
 const Activity = mongoose.model('Activity', activitySchema);
