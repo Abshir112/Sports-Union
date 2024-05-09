e9f2c0c361bde88888e64d9523e02952114e583c
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2024-05-09 18:33:45 +0200

    Added AllMembers page component
    The AllMembers page component is added to display a list of members.

---
+++ Frontend/src/pages/AllMembers.jsx
@@ -0,0 +1,50 @@
+// creeate members page here
+
+import { useState, useEffect } from 'react';
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import Member from "../components/Member";
+
+const Members = () => {
+    const [members, setMembers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchMembers = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/users');
+                if (!response.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                const data = await response.json();
+                setMembers(data);
+                setLoading(false);
+            } catch (error) {
+                setError(error.message);
+                setLoading(false);
+            }
+        }
+
+        fetchMembers();
+    }, [members]);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <Error message={error} />;
+    }
+
+    return (
+        <div>
+            <h1 style={{color: "black", backgroundColor: "#eedbc4"}} >Members</h1>
+            {members.map(member => <Member key={member._id} member={member} />)}
+        </div>
+    );
+}
+
+export default Members;
+
+    
