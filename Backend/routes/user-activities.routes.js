3b21c27c749d8735459de1acafaa34996fac65e0
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-10 11:05:02 +0200

    Fixed the routes for user activities

--- Backend/routes/user-activities.routes.js
+++ Backend/routes/user-activities.routes.js
@@ -7,13 +7,13 @@ const usersActivitiesRouter = express.Router();
 
 
 // Route handler to get all users activities
-usersActivitiesRouter.get('/users-activities/', getUserActivities);
+usersActivitiesRouter.get('/', getUserActivities);
 
 // Route handler to get a single user's activities
-usersActivitiesRouter.get('/user-activities/:id', getUserActivity);
+usersActivitiesRouter.get('/:id', getUserActivity);
 
 // Route handler to get users have the same activities
-usersActivitiesRouter.get('/users-same-activities/:id', getUsersWithSameActivity);
+usersActivitiesRouter.get('/:id', getUsersWithSameActivity);
 
 // Route handler to create a new user activity
 usersActivitiesRouter.post('/user-activity', createUserActivity);
