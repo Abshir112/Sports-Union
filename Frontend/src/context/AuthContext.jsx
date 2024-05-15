541733d47fddecbd907148cdaf14f870f1abd5b6
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-05-15 21:19:00 +0200

    chore: Add user events to AuthContext
    This commit adds the necessary code to the `AuthContext` file to handle user events. It includes a new case in the `authReducer` to set the user events, and a new `useEffect` hook to fetch user events when the user logs in.

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -25,6 +25,19 @@ export const authReducer = (state, action) => {
                 ...state,
                 userActivities: state.userActivities.filter(activity => activity.activityId !== action.payload)
             };
+        
+        case 'SET_USER_EVENTS':
+            return {
+                ...state,
+                userEvents: action.payload
+            };
+        
+        case 'REMOVE_USER_EVENT':  
+            return {
+                ...state,
+                userEvents: state.userEvents.filter(event => event._id !== action.payload)
+            }; 
+
         default:
             return state;
     }
@@ -33,7 +46,8 @@ export const authReducer = (state, action) => {
 export const AuthProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null,
-        userActivities: [] // Initialize userActivities as an empty array
+        userActivities: [], // Initialize userActivities as an empty array
+        userEvents: [] // Initialize userEvents as an empty array
     });
 
     useEffect(() => {
@@ -67,6 +81,29 @@ export const AuthProvider = ({ children }) => {
         }
     }, [state.user]);
 
+    // Fetch user events when user logs in
+    useEffect(() => {
+        if (state.user) {
+            const fetchUserEvents = async () => {
+                try {
+                    const response = await fetch(`http://localhost:3000/users-events/${state.user.user._id}`, {
+                        headers: {
+                            'Authorization': `Bearer ${state.user.token}`
+                        }
+                    });
+                    if (!response.ok) {
+                        throw new Error('Something went wrong!');
+                    }
+                    const data = await response.json();
+                    dispatch({ type: 'SET_USER_EVENTS', payload: data });
+                } catch (error) {
+                    console.error('Error fetching user events:', error);
+                }
+            };
+
+            fetchUserEvents();
+        }
+    }, [state.user]);
     console.log(state);
 
     return (
