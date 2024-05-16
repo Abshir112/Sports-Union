eb43887add6c02db2c8945458a7940e8a034751c
JwanMardini <jwan.99.mardini@gmail.com>
2024-05-16 21:46:31 +0200

    Updated Drawer component to include authentication and navigation functionality

--- Frontend/src/components/Drawer.jsx
+++ Frontend/src/components/Drawer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 import {
   Drawer,
   IconButton,
@@ -8,9 +9,58 @@ import {
   ListItemText,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-const pages = ["Home", "About", "Contact", "Members", "Stories"];
+import { useNavigate } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+
+
+
+
+
 const DrawerComp = () => {
+  const { logout } = useLogout();
+  const navigate = useNavigate();
+  const {user} = useAuthContext();
+  const userRole = user ? user.user.role : null;
   const [openDrawer, setOpenDrawer] = useState(false);
+
+  const pages = [
+    "Home",
+    "About",
+    "Activities",
+    "Events",
+    userRole === "admin" ? "Members" : null,
+    user ? "Logout" : "Login",
+  ];
+
+  const handleItemClick = (page) => {
+    console.log(`${page} clicked`);
+    switch (page) {
+      case "Home":
+        navigate("/");
+        break;
+      case "About":
+        navigate("/about");
+        break;
+      case "Activities":
+        navigate("/activities");
+        break;
+      case "Events":
+        navigate("/events");
+        break;
+      case "Members":
+        navigate("/members");
+        break;
+      case "Logout":
+        logout();
+        break;
+      case "Login":
+        navigate("/signin");
+        break;
+      default:
+        console.log("Invalid page");
+    }
+    setOpenDrawer(false); // Close the drawer after clicking an item
+  };
 
   return (
     <React.Fragment>
@@ -21,7 +71,7 @@ const DrawerComp = () => {
       >
         <List>
           {pages.map((page, index) => (
-            <ListItemButton key={index}>
+            <ListItemButton key={index} onClick={() => handleItemClick(page)}>
               <ListItemIcon>
                 <ListItemText>{page}</ListItemText>
               </ListItemIcon>
@@ -33,7 +83,7 @@ const DrawerComp = () => {
         sx={{ color: "white", marginLeft: "auto" }}
         onClick={() => setOpenDrawer(!openDrawer)}
       >
-        <MenuIcon color="white" />
+        <MenuIcon />
       </IconButton>
     </React.Fragment>
   );
