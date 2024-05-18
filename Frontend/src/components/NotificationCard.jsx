8d2faeb6e882fdb4d7f63c507bd483a1e55d7574
lakshmivishal9496 <lakshmivishal9496@gmail.com>
2024-05-18 15:16:59 +0200

    Fetched all notification from endpoint and rendered in frontend

--- Frontend/src/components/NotificationCard.jsx
+++ Frontend/src/components/NotificationCard.jsx
@@ -1,41 +1,35 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-
-
+import Box from '@mui/material/Box';
 
 const NotificationCard = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/notifications')
+      .then(response => response.json())
+      .then(data => setNotifications(data))
+      .catch(error => console.error('Error fetching notifications:', error));
+  }, []);
+
   return (
     <Card sx={{ maxWidth: 400, backgroundColor: '#2C2F33', color: 'white', padding: 2, borderRadius: 3 }}>
       <CardContent>
         <Typography variant="h5" component="div" sx={{ marginBottom: 4 }}>
           Notification
         </Typography>
-        {/* {notifications.map((notification, index) => (
+        {notifications.map((notification, index) => (
           <Box key={index} sx={{ marginBottom: 3 }}>
             <Typography variant="body1" component="div" sx={{ fontWeight: 'bold' }}>
               {notification.date}
             </Typography>
             <Typography variant="body2" component="p">
-              {notification.message} */}
-              
-            {/* </Typography>
+              {notification.description}
+            </Typography>
           </Box>
-        ))} */}
-        <Typography variant="h6" >2024-03-21</Typography>
-        <Typography variant = "body">Swimming activities announced next week would be postponed to other week.</Typography>
-        <Typography variant="h6">2024-05-29</Typography>
-        <Typography variant = "body">Watch out!! This month we limit the number of participants for the volleyball tournment. </Typography>
-        <Typography variant="h6" >2024-03-21</Typography>
-        <Typography variant = "body">Swimming activities announced next week would be postponed to other week.</Typography>
-        <Typography variant="h6">2024-05-29</Typography>
-        <Typography variant = "body">Watch out!! This month we limit the number of participants for the volleyball tournment.</Typography>
-        <Typography variant="h6" >2024-03-21</Typography>
-        <Typography variant = "body">Swimming activities announced next week would be postponed to other week.</Typography>
-        <Typography variant="h6">2024-05-29</Typography>
-        <Typography variant = "body">Watch out!! This month we limit the number of participants for the volleyball tournment.</Typography>
+        ))}
       </CardContent>
     </Card>
   );
