851747c62cd6410b63f70f03529df5c9e840822b
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-18 10:34:27 +0200

    Enhanced codebase with comprehensive JSDoc documentation for improved code readability and developer understanding.

--- Backend/routes/activity.routes.js
+++ Backend/routes/activity.routes.js
@@ -1,22 +1,66 @@
 import express from 'express';
-import { getAllActivities, createActivity, updateActivity, deleteActivity} from '../controllers/activity.controller.js';
-import requireAuth from '../middlewares/requireAuth.js'
+import { getAllActivities, createActivity, updateActivity, deleteActivity } from '../controllers/activity.controller.js';
+import requireAuth from '../middlewares/requireAuth.js';
 
 const activityRouter = express.Router();
 
-// Route handler to get all activities
+/**
+ * Route handler to get all activities.
+ * 
+ * @name GET /activities
+ * @function
+ * @memberof activityRouter
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @returns {void}
+ */
 activityRouter.get('/', getAllActivities);
 
-// Middleware to verify the token
+/**
+ * Middleware to verify the token.
+ * 
+ * This middleware ensures that the user is authenticated before proceeding to create, update, or delete activities.
+ */
 activityRouter.use(requireAuth);
 
-// Route handler to create a new activity
+/**
+ * Route handler to create a new activity.
+ * 
+ * @name POST /activities
+ * @function
+ * @memberof activityRouter
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @returns {void}
+ */
 activityRouter.post('/', createActivity);
 
-// Route handler to update an activity
+/**
+ * Route handler to update an activity.
+ * 
+ * @name PUT /activities/:id
+ * @function
+ * @memberof activityRouter
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @returns {void}
+ */
 activityRouter.put('/:id', updateActivity);
 
-// Route handler to delete an activity
+/**
+ * Route handler to delete an activity.
+ * 
+ * @name DELETE /activities/:id
+ * @function
+ * @memberof activityRouter
+ * @inner
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ * @returns {void}
+ */
 activityRouter.delete('/:id', deleteActivity);
 
 export default activityRouter;
