4d6af3f3570c65f169e27ab2690d2b04390939d6
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-05-21 11:31:18 +0200

    Refactored useAddActivity hook to handle loading and error states consistently

--- Frontend/src/hooks/useAddActivity.jsx
+++ Frontend/src/hooks/useAddActivity.jsx
@@ -1,48 +1,41 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
 
-
 const useAddActivity = () => {
     const [addingActivityError, setError] = useState(null);
-    const [addingActivityIsLoading, setIsLoading] = useState(null);
+    const [addingActivityIsLoading, setIsLoading] = useState(false);
     const { user } = useAuthContext();
 
-    const addActivity = (activityData) => {
+    const addActivity = async (activityData) => {
+        setIsLoading(true);
+        setError(null);
+        
         try {
-            setIsLoading(true);
             const requestData = { ...activityData, currentParticipants: 0 };
-            fetch('https://sports-union.onrender.com/api/v1/activities', {
+            const response = await fetch('https://sports-union.onrender.com/api/v1/activities', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${user.token}`
                 },
                 body: JSON.stringify(requestData)
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to add activity');
-                }
-                setIsLoading(false);
-                setError(null);
-                // Handle successful addition of activity (if needed)
-                return response.json();
-            })
-            .then(_ => {
-                window.location.reload();
-            })
+            });
 
+            if (!response.ok) {
+                throw new Error('Failed to add activity');
+            }
+
+            const result = await response.json();
+            // Handle successful addition of activity (if needed)
+            return result;
         } catch (error) {
             setError(error.message);
+        } finally {
             setIsLoading(false);
         }
-        finally {
-            setIsLoading(false);
-        }
-    }
+    };
 
     return { addActivity, addingActivityError, addingActivityIsLoading };
-}
-
+};
 
 export default useAddActivity;
